c37550f272232f9cc2a9f710f74c712db1264666
liondancing <133041194+liondancing@users.noreply.github.com>
2023-09-03 01:38:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ const buildQuery = (props, siteDetails) => {
 };
 
 function App(props) {
-  // setup proxy privider
+  // setup proxy
   const serverProxyDomain = checkProxy();
 
   useEffect(() => {
